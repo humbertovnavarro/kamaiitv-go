bafc45a8751737c350bcf9748ee500f6b6e72600
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-02-01 11:41:35 -0800

    1/2/22.1

--- routes/tokens.go
+++ routes/tokens.go
@@ -1,9 +1,6 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"github.com/gwuhaolin/livego/configure"
@@ -31,35 +28,8 @@ func signToken(id string, ip string, agent string) (string, error) {
 	tokenString, err := token.SignedString(secret)
 	return tokenString, err
 }
-func verifyToken(tokenString string) (*jwt.Token, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return secret, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return token, nil
-}
-
-func extractToken(c *gin.Context) (*jwt.Token, error) {
-	tokenString := c.GetHeader("Authorization")
-	token, err := verifyToken(tokenString)
-	token.Claims["id"]
-	return token, err
-}
 
 func tokenAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString := c.GetHeader("Authorization")
-		_, err := verifyToken(tokenString)
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, err.Error())
-			c.Abort()
-			return
-		}
-		c.Next()
 	}
 }
